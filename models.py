334452ceabf311b1f92352adb775bb96acece0d1
morrillo <gustavo.orrillo@moldeo.coop>
2016-06-20 11:49:24 -0300

    Removed email buttons

--- models.py
+++ models.py
@@ -10,6 +10,7 @@ _logger = logging.getLogger(__name__)
 class crm_helpdesk(models.Model):
 	_inherit = 'crm.helpdesk'
 
+	"""
 	@api.multi
 	def email_supplier(self):
                 return {'type': 'ir.actions.act_window',
@@ -33,7 +34,7 @@ class crm_helpdesk(models.Model):
                         'target': 'new',
                         'nodestroy': True,
                         }
-
+	"""
 
 	@api.one
 	def _compute_supplier_id(self):
