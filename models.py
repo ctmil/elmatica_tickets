85722799c8a209fda876015a1ad98af4bbf6c113
morrillo <gustavo.orrillo@moldeo.coop>
2016-08-26 12:46:43 -0300

    Changed e-mail to customer form

--- models.py
+++ models.py
@@ -72,21 +72,61 @@ class crm_helpdesk(models.Model):
 
 	@api.multi
 	def email_customer(self):
-		vals = {
-			'subject': self.name,
-			'body': self.name,
-			}
-		ticket_email_customer = self.env['ticket.email.customer'].create(vals)
-                return {'type': 'ir.actions.act_window',
-                        'name': 'e-mail Customer',
-                        'res_model': 'ticket.email.customer',
-                        'view_type': 'form',
-                        'view_mode': 'form',
-                        #'view_id': view_id,
-			'res_id': ticket_email_customer.id,
-                        'target': 'new',
-                        'nodestroy': True,
-                        }
+		title_window = self._context.get('title_window', _('Comment'))
+
+		assert len(self) == 1, 'This option should only be used for a single id at a time.'
+
+		compose_form = self.env.ref('mail.email_compose_message_wizard_form', False)
+		#if self.supplier_id:
+		#	vals_message = {
+		#		'res_id': self.id,
+		#		'res_model': 'crm.helpdesk',
+		#		'partner_ids': (6,0,[self.supplier_id.id])
+		#		}
+		#	message_id = self.env['mail.compose.message'].create(vals_message)
+		if self.customer_id.email:
+			ctx = dict(
+				default_model='crm.helpdesk',
+				default_res_id=self.id,
+				default_email_to=self.customer_id.email,
+				default_partner_ids=[self.customer_id.id],
+				#default_use_template=bool(template),
+				#default_template_id=template.id,
+				default_composition_mode='comment',
+				#mark_invoice_as_sent=False,
+				#default_is_log=True,
+				#is_log=True,
+				#internal_partners_only=True,
+			       )	
+		return {
+	        	    'name': title_window,
+	        	    'type': 'ir.actions.act_window',
+		            'view_type': 'form',
+        		    'view_mode': 'form',
+		            #'res_model': 'elmatica_invoice.mail.compose.message', # 'compose.message', # 'mail.compose.message',
+			    #'res_id': message_id.id,
+        		    'res_model': 'mail.compose.message',
+	        	    'views': [(compose_form.id, 'form')],
+	        	    'view_id': compose_form.id,
+		            'target': 'new',
+        		    'context': ctx,
+		        }
+
+		#vals = {
+		#	'subject': self.name,
+		#	'body': self.name,
+		#	}
+		#ticket_email_customer = self.env['ticket.email.customer'].create(vals)
+                #return {'type': 'ir.actions.act_window',
+                #        'name': 'e-mail Customer',
+                #        'res_model': 'ticket.email.customer',
+                #        'view_type': 'form',
+                #        'view_mode': 'form',
+                #        #'view_id': view_id,
+		#	'res_id': ticket_email_customer.id,
+                #        'target': 'new',
+                #        'nodestroy': True,
+                #        }
 	@api.one
 	def _compute_supplier_id(self):
 		if self.purchase_id:
