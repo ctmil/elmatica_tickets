ebdb3c16158b0c4fb40213e8683887ee01a7eef8
morrillo <gustavo.orrillo@moldeo.coop>
2016-09-05 17:27:21 -0300

    I think this is the final hit

--- models_old.py
+++ models_old.py
@@ -17,38 +17,60 @@ class mail_compose_message(osv.TransientModel):
     """
     _inherit = 'mail.compose.message'
 
-    def get_record_data(self, cr, uid, values, context=None):
-        """ Returns a defaults-like dict with initial values for the composition
-        wizard when sending an email related a previous email (parent_id) or
-        a document (model, res_id). This is based on previously computed default
-        values. """
+    def default_get(self, cr, uid, fields, context=None):
+        """ Handle composition mode. Some details about context keys:
+            - comment: default mode, model and ID of a record the user comments
+                - default_model or active_model
+                - default_res_id or active_id
+            - reply: active_id of a message the user replies to
+                - default_parent_id or message_id or active_id: ID of the
+                    mail.message we reply to
+                - message.res_model or default_model
+                - message.res_id or default_res_id
+            - mass_mail: model and IDs of records the user mass-mails
+                - active_ids: record IDs
+                - default_model or active_model
+        """
         if context is None:
             context = {}
-        result, subject = {}, False
-        if values.get('parent_id'):
-            parent = self.pool.get('mail.message').browse(cr, uid, values.get('parent_id'), context=context)
-            result['record_name'] = parent.record_name,
-            subject = tools.ustr(parent.subject or parent.record_name or '')
-            if not values.get('model'):
-                result['model'] = parent.model
-            if not values.get('res_id'):
-                result['res_id'] = parent.res_id
-            partner_ids = values.get('partner_ids', list()) + [partner.id for partner in parent.partner_ids]
-            if context.get('is_private') and parent.author_id:  # check message is private then add author also in partner list.
-                partner_ids += [parent.author_id.id]
-            result['partner_ids'] = partner_ids
-        elif values.get('model') and values.get('res_id'):
-            doc_name_get = self.pool[values.get('model')].name_get(cr, uid, [values.get('res_id')], context=context)
-            result['record_name'] = doc_name_get and doc_name_get[0][1] or ''
-            subject = tools.ustr(result['record_name'])
+        result = super(mail_compose_message, self).default_get(cr, uid, fields, context=context)
+        # v6.1 compatibility mode
+        result['composition_mode'] = result.get('composition_mode', context.get('mail.compose.message.mode', 'comment'))
+        result['model'] = result.get('model', context.get('active_model'))
+        result['res_id'] = result.get('res_id', context.get('active_id'))
+        result['parent_id'] = result.get('parent_id', context.get('message_id'))
 
-        re_prefix = _('Re:')
-        if subject and not (subject.startswith('Re:') or subject.startswith(re_prefix)):
-            subject = "%s %s" % (re_prefix, subject)
-        result['subject'] = subject
-	if context.has_key('default_model') and context.has_key('elmatica_tickets'):
-		if context['default_model'] == 'crm.helpdesk' and context['elmatica_tickets'] == True:
-			result['attachment_ids'] = None
+        if not result['model'] or not self.pool.get(result['model']) or not hasattr(self.pool[result['model']], 'message_post'):
+            result['no_auto_thread'] = True
+
+        # default values according to composition mode - NOTE: reply is deprecated, fall back on comment
+        if result['composition_mode'] == 'reply':
+            result['composition_mode'] = 'comment'
+        vals = {}
+        if 'active_domain' in context:  # not context.get() because we want to keep global [] domains
+            vals['use_active_domain'] = True
+            vals['active_domain'] = '%s' % context.get('active_domain')
+        if result['composition_mode'] == 'comment':
+            vals.update(self.get_record_data(cr, uid, result, context=context))
+
+        for field in vals:
+            if field in fields:
+                result[field] = vals[field]
+        # TDE HACK: as mailboxes used default_model='res.users' and default_res_id=uid
+        # (because of lack of an accessible pid), creating a message on its own
+        # profile may crash (res_users does not allow writing on it)
+        # Posting on its own profile works (res_users redirect to res_partner)
+        # but when creating the mail.message to create the mail.compose.message
+        # access rights issues may rise
+        # We therefore directly change the model and res_id
+        if result['model'] == 'res.users' and result['res_id'] == uid:
+            result['model'] = 'res.partner'
+            result['res_id'] = self.pool.get('res.users').browse(cr, uid, uid).partner_id.id
+        if fields is not None:
+            [result.pop(field, None) for field in result.keys() if field not in fields]
+	if context.has_key('elmatica_tickets'):
+	    result['attachment_ids'] = None
         return result
+
 
 mail_compose_message()
